f2eabe28d0b1778bf1a3f4b947a0621bc273aa7a
Domenico Iezzi <domenico.iezzi.201@gmail.com>
2019-03-13 15:39:08 +0100

    Aggiunti esempi per segnali

---
+++ code/E01 - Process/3-signal/sign-2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+void custom_alarm( int seconds ) {
+    // sleep per 10 secondi
+    sleep(seconds);
+    kill(getpid(), SIGALRM);
+}
+
+void alarm_handler( int sig ) {
+    if (sig == SIGALRM) {
+        printf( "Allarme ricevuto, sono passati 10 secondi\n" );
+        exit( 1 );
+    }
+}
+
+int main() {
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa));
+    sa.sa_handler = &alarm_handler;
+
+
+    // Registro l'handler
+    if ( sigaction(SIGALRM, &sa, NULL) != 0 ) {
+        printf( "Errore nel registrare l'handler per SIGALARM\n" );
+        exit( 1 );
+    } 
+
+    printf( "Ho impostato l'allarme, scatterà tra 10 secondi\n" );
+    custom_alarm( 10 ); 
+
+    while(1);
+
+    return 0;
+}
+
